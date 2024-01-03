e540b394b032586d0c91111614bd49a24534683a
Ramy-Badr Ahmed <ramy.ahmed@dagstuhl.de>
2024-01-03 15:36:47 +0100

    Imprint & Privacy Links

--- resources/views/pages/general/imprint.blade.php
+++ resources/views/pages/general/imprint.blade.php
@@ -16,11 +16,11 @@
         66687 Wadern<br>
         Deutschland<br>
         <br>
-        Phone +49 - 6871 - 905 0<br>
-        Fax  +49 - 6871 - 905 133<br>
+        Phone <a href="tel:+4968719050">+49 - 6871 - 905 0</a><br>
+        Fax  <a href="tel:+496871905133">+49 - 6871 - 905 133</a><br>
         <br>
-        E-Mail: service(at)dagstuhl.de<br>
-        Internet: www.dagstuhl.de<br>
+        E-Mail: <a href="mailto:service@dagstuhl.de">service(at)dagstuhl.de</a><br>
+        Internet: <a href="https://dagstuhl.de/" class="uri" target='_blank' rel='noopener noreferrer'>www.dagstuhl.de</a><br>
         <br>
         Scientific Director: Prof. Dr. Raimund Seidel<br>
         Administrative Director: Heike Mei&szlig;ner<br>
@@ -32,11 +32,11 @@
         <b>Disclaimer of Liability</b>
         <br><br>
         By using our web pages you acknowledge the following disclaimer of liability:
-        Apart from offering content of our own and hyperlinks to third-party web sites, our service also makes publications available for which the authors of the publications are solely responsible.<br>
-        The web site content is provided "as-is", i.e. the web site owners assume no liability for the up-to-dateness, correctness, completeness or quality of the information provided on the web site. No liability whatsoever shall be assumed for claims lodged against the owners following from material or immaterial damage or loss as the result of the use of or the inability to use the information offered or the use of erroneous or incomplete information, unless premeditation or gross negligence on the part of the web site owners can be demonstrated.
-        All web site offerings are non-binding and at no obligation to the owners. The owners expressly reserve the right to modify, supplement or delete parts of pages or the entire web site offerings without notice, or temporarily or permanently discontinue publication of content (with the exception of the publications on our online publication server).<br>
-        Only publications and links are included whose content in the fair judgment of the web site owners does not violate applicable German law at the time of inclusion. However, despite careful examination and taking every precaution it cannot be precluded that individual web pages or documents may contain content which is subject to criminal prosecution or directly or indirectly point to pages containing criminal content.
-        Consequently, the institution operating the web site and its personnel assume no liability whatsoever for the content of the publications made available by it or on external web pages. Nor is any liability assumed for any false statements, misrepresentations or legal offenses in the publications made available on the web site or on external web pages quoted. The opinions and/or claims expressed in the publications provided or on linked web pages are the sole responsibility of the authors expressing them.
+        Apart from offering content of our own and hyperlinks to third-party websites, our service also makes publications available for which the authors of the publications are solely responsible.<br>
+        The website content is provided "as-is", i.e. the website owners assume no liability for the up-to-dateness, correctness, completeness or quality of the information provided on the website. No liability whatsoever shall be assumed for claims lodged against the owners following from material or immaterial damage or loss as the result of the use of or the inability to use the information offered or the use of erroneous or incomplete information, unless premeditation or gross negligence on the part of the website owners can be demonstrated.
+        All website offerings are non-binding and at no obligation to the owners. The owners expressly reserve the right to modify, supplement or delete parts of pages or the entire website offerings without notice, or temporarily or permanently discontinue publication of content (with the exception of the publications on our online publication server).<br>
+        Only publications and links are included whose content in the fair judgment of the website owners does not violate applicable German law at the time of inclusion. However, despite careful examination and taking every precaution it cannot be precluded that individual web pages or documents may contain content which is subject to criminal prosecution or directly or indirectly point to pages containing criminal content.
+        Consequently, the institution operating the website and its personnel assume no liability whatsoever for the content of the publications made available by it or on external web pages. Nor is any liability assumed for any false statements, misrepresentations or legal offenses in the publications made available on the website or on external web pages quoted. The opinions and/or claims expressed in the publications provided or on linked web pages are the sole responsibility of the authors expressing them.
         <br><br>
     </p>
     <p>
