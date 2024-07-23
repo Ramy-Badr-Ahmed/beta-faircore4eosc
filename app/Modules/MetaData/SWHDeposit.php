4702dc2e3117d2ad79e8421760b346e38c7d8ed5
Ramy-Badr-Ahmed <126559907+Ramy-Badr-Ahmed@users.noreply.github.com>
2024-07-23 13:10:15 +0200

    Update SWHDeposit.php

--- app/Modules/MetaData/SWHDeposit.php
+++ app/Modules/MetaData/SWHDeposit.php
@@ -2,8 +2,8 @@
 
 /**
  * @Author: Ramy-Badr-Ahmed
- * @Desc: LZI -- SWH Deposit
- * @Repo: https://github.com/dagstuhl-publishing/beta-faircore4eosc
+ * @Desc: SWH Deposit
+ * @Repo: https://github.com/Ramy-Badr-Ahmed/beta-faircore4eosc
  */
 
 namespace App\Modules\MetaData;
